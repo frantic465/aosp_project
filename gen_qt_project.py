d68550dd1a6d7d4cae10cb73bdc28287b09341cc
Andrey Vakhrushev <v.andrew.465@gmail.com>
2016-01-02 21:30:51 +0300

    QtCreator project generator

---
+++ gen_qt_project.py
@@ -0,0 +1,50 @@
+#/usr/bin/python
+
+
+import os
+import os.path
+
+base_dir = "../aosp"
+
+include_paths = []
+extra_include_paths = set()
+headers = []
+sources = []
+
+root_dirs = ['abi', 'frameworks', 'hardware', 'packages', 'system', 'bionic', 'libcore', 'libnativehelper']
+
+for root_dir in root_dirs:
+    for root, dirs, files in os.walk(os.path.join(base_dir, root_dir)):
+        root = root.replace(base_dir, "", 1)
+        for name in files:
+            (bname, ext) = os.path.splitext(name)
+            if ext == '.h':
+                headers.append(os.path.join(root, name))
+                if not '/include' in root:
+                    extra_include_paths.add(root)
+            elif ext == '.cpp' or ext == '.c' or ext == '.cc':
+                sources.append(os.path.join(root, name))    
+
+        for name in dirs:
+            if name == 'include':
+                include_paths.append(os.path.join(root, name))
+
+print """
+QT -= core gui
+TARGET = android
+TEMPLATE = app
+"""
+
+print "ROOT_DIR = " + base_dir
+
+for include_path in include_paths:
+    print "INCLUDEPATH += $${ROOT_DIR}" + include_path
+
+for include_path in extra_include_paths:
+    print "INCLUDEPATH += $${ROOT_DIR}" + include_path
+
+for header in headers:
+    print "HEADERS += $${ROOT_DIR}" + header
+
+for source in sources:
+    print "SOURCES += $${ROOT_DIR}" + source
